266caf79b5f6965589e91dc81e8ff5378ba4eca2
Arian <as228xs@student.lnu.se>
2025-11-22 02:20:47 +0100

    Add index.js files for analyzers and validators folders
    These files are required for Node.js module resolution when using require('./analyzers') and require('./validators'). Without them, Node.js cannot find the modules in these folders.
    
    )

---
+++ src/analyzers/index.js
@@ -0,0 +1,13 @@
+const LineAnalyzer = require('./LineAnalyzer')
+const CommentAnalyzer = require('./CommentAnalyzer')
+const ComplexityAnalyzer = require('./ComplexityAnalyzer')
+const FunctionAnalyzer = require('./FunctionAnalyzer')
+const QualityAnalyzer = require('./QualityAnalyzer')
+
+module.exports = {
+  LineAnalyzer,
+  CommentAnalyzer,
+  ComplexityAnalyzer,
+  FunctionAnalyzer,
+  QualityAnalyzer
+}
---
+++ src/validators/index.js
@@ -0,0 +1,9 @@
+const BracketValidator = require('./BracketValidator')
+const SyntaxValidator = require('./SyntaxValidator')
+const CodeStyleValidator = require('./CodeStyleValidator')
+
+module.exports = {
+  BracketValidator,
+  SyntaxValidator,
+  CodeStyleValidator
+}
